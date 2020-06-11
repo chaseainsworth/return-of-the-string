4e401215f92b821efa01ed77ea17b75850269380
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-06-11 07:30:45 -0400

    all 13 passed

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(exclaim) {
+  return exclaim + "!";
+}
 
+function getFirstCharacter(begin) {
+  return begin[0];
+}
 
+function getLastCharacter(end) {
+  return end[end.length - 1];
+}
 
+function getOneCharacter(single, x) {
+  return single[single.length = x];
+}
 
+function getTwoCharacters(double, y, z) {
+  return double[double.length = y] + double[double.length = z];
+}
+
+function makeCapitalized(bigWords) {
+  return bigWords.toUpperCase(bigWords);
+}
+
+function yellLouder(scream) {
+  return scream.toUpperCase(scream) + "!!!";
+}
+
+function getInitials(name) {
+  return name[name.length = 0] + "." + name[name.indexOf(" ") + 1] + ".";
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
